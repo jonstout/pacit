02eda648d40af1c8f4262eeb15cce3080eebb2c8
Jonathan M Stout <jonstout@indiana.edu>
2013-04-08 21:29:59 -0400

    initial read write methods for arp

--- arp.go
+++ arp.go
@@ -13,3 +13,67 @@ type ARP struct {
 	IPDst uint32
 }
 
+func (a *Arp) Read(b []byte) (n int, err error) {
+	n, err := Ethernet.Read(b)
+	if n == 0 {
+		return
+	}
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.BigEndian, e.HWType)
+	binary.Write(buf, binary.BigEndian, e.ProtoType)
+	binary.Write(buf, binary.BigEndian, e.HWLength)
+	binary.Write(buf, binary.BigEndian, e.ProtoLength)
+	binary.Write(buf, binary.BigEndian, e.Operation)
+	binary.Write(buf, binary.BigEndian, e.HWSrc)
+	binary.Write(buf, binary.BigEndian, e.IPSrc)
+	binary.Write(buf, binary.BigEndian, e.HWDst)
+	binary.Write(buf, binary.BigEndian, e.IPDst)
+	n, err = buf.Read(b)
+	return
+}
+
+func (a *Arp) Write(b []byte) (n int, err error) {
+	n, err := Ethernet.Write(b)
+	if n == 0 {
+		return
+	}
+	buf := bytes.NewBuffer(b[n:])
+	if err = binary.Read(buf, binary.BigEndian, &a.HWType); err != nil {
+		return
+	}
+	n += 2
+	if err = binary.Read(buf, binary.BigEndian, &a.ProtoType); err != nil {
+		return
+	}
+	n += 2
+	if err = binary.Read(buf, binary.BigEndian, &a.HWLength); err != nil {
+		return
+	}
+	n += 1
+	if err = binary.Read(buf, binary.BigEndian, &a.ProtoLength); err != nil {
+		return
+	}
+	n += 1
+
+	if err = binary.Read(buf, binary.BigEndian, &a.Operation); err != nil {
+		return
+	}
+	n += 2
+		if err = binary.Read(buf, binary.BigEndian, &a.HWSrc); err != nil {
+		return
+	}
+	n += 6
+		if err = binary.Read(buf, binary.BigEndian, &a.IPSrc); err != nil {
+		return
+	}
+	n += 4
+		if err = binary.Read(buf, binary.BigEndian, &a.HWDst); err != nil {
+		return
+	}
+	n += 6
+		if err = binary.Read(buf, binary.BigEndian, &a.IPDst); err != nil {
+		return
+	}
+	n += 4
+	return
+}
