7dfa8d981d22642e56e6eae21bde803721e8766c
Jonathan M Stout <jonstout@indiana.edu>
2013-04-08 22:02:05 -0400

    adding subtype ids

--- lldp.go
+++ lldp.go
@@ -16,6 +16,30 @@ type ChassisTLV struct {
 
 }
 
+// Chassis ID subtypes
+const (
+	_ = iota
+	CHASSIS_COMPONENT
+	IFACE_ALIAS
+	PORT_COMPONENT
+	MAC_ADDR
+	NET_ADDR
+	IFACE_NAME
+	LOCAL_ASSGN
+)
+
+// Port ID subtypes
+const (
+	_ = iota
+	IFACE_ALIAS
+	PORT_COMPONENT
+	MAC_ADDR
+	NET_ADDR	
+	IFACE_NAME
+	CIRCUIT_ID
+	LOCAL_ASSGN
+)
+
 type PortTLV struct {
 
 }
