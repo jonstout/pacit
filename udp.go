ceef50e0516a4c01e34b589b61c0702e6b3c13fd
Vasiliy Tolstov <v.tolstov@selfip.ru>
2013-08-13 10:47:27 +0400

    add udp ip packet type, gofmt sources
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

---
+++ udp.go
@@ -0,0 +1,58 @@
+package pacit
+
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+)
+
+type UDP struct {
+	PortSrc  uint16
+	PortDst  uint16
+	Length   uint16
+	Checksum uint16
+	Data     []byte
+}
+
+func (u *UDP) Len() (n uint16) {
+	return uint16(8 + len(u.Data))
+}
+
+func (u *UDP) Read(b []byte) (n int, err error) {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.BigEndian, u.PortSrc)
+	binary.Write(buf, binary.BigEndian, u.PortDst)
+	binary.Write(buf, binary.BigEndian, u.Length)
+	binary.Write(buf, binary.BigEndian, u.Checksum)
+	binary.Write(buf, binary.BigEndian, u.Data)
+	if n, err = buf.Read(b); n == 0 {
+		return
+	}
+	return n, io.EOF
+}
+
+func (u *UDP) Write(b []byte) (n int, err error) {
+	buf := bytes.NewBuffer(b)
+	if err = binary.Read(buf, binary.BigEndian, &u.PortSrc); err != nil {
+		return
+	}
+	n += 2
+	if err = binary.Read(buf, binary.BigEndian, &u.PortDst); err != nil {
+		return
+	}
+	n += 2
+	if err = binary.Read(buf, binary.BigEndian, &u.Length); err != nil {
+		return
+	}
+	n += 2
+	if err = binary.Read(buf, binary.BigEndian, &u.Checksum); err != nil {
+		return
+	}
+	n += 2
+	u.Data = make([]byte, len(b)-n)
+	if err = binary.Read(buf, binary.BigEndian, &u.Data); err != nil {
+		return
+	}
+	n += len(u.Data)
+	return
+}
