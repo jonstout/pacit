8b931761108781551b954d4f8a37d7ad5bf46312
Vasiliy Tolstov <v.tolstov@selfip.ru>
2013-09-09 10:33:04 +0400

    read magick
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- udp.go
+++ udp.go
@@ -81,19 +81,13 @@ func (u *UDP) Write(b []byte) (n int, err error) {
 		return
 	}
 	n += 2
-	/*
-		if u.Length >= 8 {
-			u.Data = make([]byte, u.Length-8)
-		}
-		if u.Length == 0 {
-			u.Data = make([]byte, buf.Len())
-		}
-
-		if err = binary.Read(buf, binary.BigEndian, &u.Data); err != nil {
-			return
-		}
-	*/
-	n += copy(u.Data, b[n:])
-	//	n += len(u.Data)
+	if u.Length >= 8 {
+		u.Data = make([]byte, u.Length-8)
+	}
+	if u.Length == 0 {
+		u.Data = make([]byte, buf.Len())
+	}
+	m, err := io.ReadFull(buf, u.Data)
+	n += m
 	return
 }
